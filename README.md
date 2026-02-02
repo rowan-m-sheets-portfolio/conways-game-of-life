d2785b850310a3c1b8e5a69fd92cb5a49029f15f
Aahz02 <powerclaw2002@gmail.com>
2026-02-02 12:36:50 -0800

    finish implementation, clean up use statements, update README

--- README.md
+++ README.md
@@ -6,5 +6,14 @@ main.rs: Rowan Sheets\n
 life.rs: provided by Bart Massey
 
 ## How to run
-1. Make sure you have all the requirements to use embedded rust with the BBC micro:bit (instructions available [here](https://docs.rust-embedded.org/discovery-mb2/index.html))
+1. Make sure you have all the requirements to use embedded rust with the BBC micro:bit (instructions available
+[here](https://docs.rust-embedded.org/discovery-mb2/index.html))
 2. Run the command `cargo embed`
+3. The A button on the microbit can be used to rapidly re-generate a new board state, the B button can be used to
+invert the current board state
+
+## Implementation
+The program works by seeding a nanorand random number generator with a random number taken from the microbit's hardware
+random number generator to ensure randomness without repeatedly using the costly hardware rng. This nanorand rng is then used
+to create an inital board state which then has the rules of the game of life applied to it and is displayed at a framerate of 10 fps.
+If the board is ever filled with all "dead" cells, it will wait 5 frames and then re-randomize.
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,28 @@
 #![no_main]
 #![no_std]
 
-use microbit::{display, pac::acl::acl::perm};
 use panic_rtt_target as _;
-use rtt_target::{rprintln, rtt_init_print};
+use rtt_target::rtt_init_print;
 
 use cortex_m_rt::entry;
 use embedded_hal::{delay::DelayNs, digital::InputPin};
 #[rustfmt::skip]
 use microbit::{
-    board::{Board, Buttons},
+    board::{Board},
     display::blocking::Display,
     hal::{
         Rng as HwRng,
         timer::Timer,
     },
 };
-use nanorand::{pcg64::Pcg64, Rng, SeedableRng};
+use nanorand::{pcg64::Pcg64, Rng};
 
 mod life;
 use life::*;
 
 #[entry]
 fn main() -> ! {
+    rtt_init_print!();
     let board = Board::take().unwrap();
 
     let mut hw_rng = HwRng::new(board.RNG);
@@ -32,10 +32,21 @@ fn main() -> ! {
     let mut timer1 = Timer::new(board.TIMER1);
 
     let mut display = Display::new(board.display_pins);
+    let mut button_a = board.buttons.button_a;
+    let mut button_b = board.buttons.button_b;
 
     let mut state: [[u8; 5]; 5] = [[0; 5]; 5];
     randomize_state(&mut state, &mut rng);
     loop {
+        if button_a.is_low().unwrap() {
+            while button_a.is_low().unwrap() {
+                randomize_state(&mut state, &mut rng);
+                display.show(&mut timer0, state, 10);
+            }
+        } else if button_b.is_low().unwrap() && timer1.read() == 0 {
+            invert(&mut state);
+            timer1.start(500_000);
+        }
         if !done(&state) {
             life(&mut state);
             display.show(&mut timer0, state, 100);
@@ -50,6 +61,18 @@ fn randomize_state(state: &mut [[u8; 5]; 5], rng: &mut Pcg64) {
     for row in state {
         for index in row {
             *index = rng.generate_range(0_u8..=1);
+        }
+    }
+}
+
+fn invert(state: &mut [[u8; 5]; 5]) {
+    for row in state {
+        for index in row {
+            *index = if *index == 0 {
+                1
+            } else {
+                0
+            }
         }
     }
 }
